72b681d2ab49155038a5cad0167ab827336fe0d2
Dmitri Antyfeev <60137521+Antyfeev96@users.noreply.github.com>
2021-01-03 22:53:26 +0300

    It works, questions in app.test.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,3 @@
-const object = {
-  name: 'мечник',
-  health: 10,
-  level: 2,
-  attack: 80,
-  defence: 40,
-};
-
 const sortFunc = (obj, [a, b]) => {
   const startArr = Object.entries(obj);
   const arr = [];
